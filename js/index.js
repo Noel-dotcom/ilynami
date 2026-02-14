627b341dbec1b2f400e96951d2d7f1e5dda5b874
Noel-dotcom <noelhannesdelun@gmail.com>
2026-02-14 14:25:14 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Hope You Like Flowers`.split('')
+const text = `Hand Picked For You`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
